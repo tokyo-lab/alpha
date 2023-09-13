6f2fe818df4e22a7edefac5e0c88d9f40589bccd
cooper <tokyolabs@protonmail.com>
2023-09-13 10:47:00 -0400

    wip

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
 from alpha_vantage.timeseries import TimeSeries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import io
+import json
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+api_key = config.get("api_key")
+print("apik key: ", api_key)
+
+ts1 = TimeSeries(key=api_key)
+
+# Retrieve the monthly time series data for AAPL
+data, meta_data = ts1.get_monthly("AAPL")
+
+# Print the data
+print("Monthly Time Series Data for AAPL:")
+print(data)
+
+# Optionally, you can print the metadata as well
+print("Meta Data:")
+print(meta_data)
