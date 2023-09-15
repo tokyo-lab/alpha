c4827f6543b74e2834dfcc06d2e3c070747a7082
cooper <tokyolabs@protonmail.com>
2023-09-15 17:35:36 -0400

    wip

--- explore.py
+++ explore.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import pandas as pd
 
 # Load your API key from a configuration file (config.json in this case)
 with open("config.json", "r") as config_file:
@@ -10,24 +9,27 @@ api_key = config.get("api_key")
 # print("API key: ", api_key)
 
 # Load the list of stock tickers from a JSON file
-# Load the list of stock tickers from a JSON file
 with open("tickers.json", "r") as tickers_file:
     data = json.load(tickers_file)
     print("data: ", data)
 
     tickers = data["tickers"]
     print("tickers: ", tickers)
-# Initialize an empty DataFrame to store the stock data
-all_data = pd.DataFrame()
+# Create an empty dictionary to store the stock data
+all_stock_data = {}
 
 # Loop through each ticker and fetch data
 for ticker in tickers:
     # Construct the API request URL for each ticker
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={ticker}&apikey={api_key}&datatype=json"
 
-    # Send the request and decode the response
-    r = requests.get(url)
-    # data = pd.read_csv(io.StringIO(r.decode("utf-8")))
-    data = r.json()
+    #  Send the request and parse the JSON response
+    response = requests.get(url)
+    stock_data = response.json()
 
-    print(data)
+    # Add the stock data to the dictionary with the ticker symbol as the key
+    all_stock_data[ticker] = stock_data
+
+# Save all_stock_data as a JSON file
+with open("stock_data.json", "w") as json_file:
+    json.dump(all_stock_data, json_file, indent=4)
