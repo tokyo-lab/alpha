69c5906e7320312b914c0a9aad8dc6c96f572737
cooper <tokyolabs@protonmail.com>
2023-09-16 21:53:24 -0400

    wip

--- alpha_app.py
+++ alpha_app.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 
 class RunThis:
@@ -8,48 +9,34 @@ class RunThis:
         self.config_file_path = config_file_path
         print("stock_data from class: ", stock_file_path)
 
+    def read_json(self, file_path):
+        try:
+            with open(file_path, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print(f"{file_path} not found")
+            return None
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON in {file_path}")
+            return None
+
     def StartApp(self):
-        try:
-            # Load the list of stock tickers from a JSON file
-            with open(self.stock_file_path, "r") as tickers_file:
-                data = json.load(tickers_file)
-                # print("data from StartApp function: ", data)
-                tickers = data["tickers"]
-                print("tickers: ", tickers)
-        except FileNotFoundError:
-            print("Stock file not found")
-            return
-        except json.JSONDecodeError:
-            print("Error decoding JSON in stock file")
-            return
-            # Create an empty dictionary to store the stock data
+        data = self.read_json(self.stock_file_path)
+        if data is None:
+            return None
+        tickers = data["tickers"]
 
-        # Load your API key from a configuration file (config.json in this case)
-        try:
-            with open(self.config_file_path, "r") as config_file:
-                config = json.load(config_file)
-                api_key = config.get("api_key")
-        except FileNotFoundError:
-            print("Configuration file not found")
-            return
-        except json.JSONDecodeError:
-            print("Error decoding JSON in config file")
-            return
-        # print("API key: ", api_key)
+        config = self.read_json(self.config_file_path)
+        if config is None:
+            return None
+        api_key = config.get("api_key")
+
         all_stock_data = {}
-
-        # Loop through each ticker and fetch data
         for ticker in tickers:
-            # Construct the API request URL for each ticker
             url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={ticker}&apikey={api_key}&datatype=json"
             print("url: ", url)
-            #  Send the request and parse the JSON response
             response = requests.get(url)
-            stock_data = response.json()
-            print("stock_data", stock_data)
-
-            # Add the stock data to the dictionary with the ticker symbol as the key
-            all_stock_data[ticker] = stock_data
+            all_stock_data[ticker] = response.json()
 
         return all_stock_data
 
@@ -82,5 +69,6 @@ class StockDataAnalyzer(RunThis):
 # Example of usage
 app = RunThis(stock_file_path="tickers.json", config_file_path="config.json")
 all_stock_data = app.StartApp()
-analyzer = StockDataAnalyzer(all_stock_data, "tickers.json", "config.json")
-analyzer.get_last_n_weeks_close_prices("TSLA", 2)
+if all_stock_data:
+    analyzer = StockDataAnalyzer(all_stock_data, "tickers.json", "config.json")
+    analyzer.get_last_n_weeks_close_prices("MSFT", 2)
