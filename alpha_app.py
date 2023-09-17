524a549e634a7686a3d30eb95e869dc376fb3ce1
cooper <tokyolabs@protonmail.com>
2023-09-16 21:28:05 -0400

    fixed bugs

--- alpha_app.py
+++ alpha_app.py
@@ -9,13 +9,19 @@ class RunThis:
         print("stock_data from class: ", stock_file_path)
 
     def StartApp(self):
-        # Load the list of stock tickers from a JSON file
-        with open(self.stock_file_path, "r") as tickers_file:
-            data = json.load(tickers_file)
-            # print("data from StartApp function: ", data)
-            tickers = data["tickers"]
-            print("tickers: ", tickers)
-
+        try:
+            # Load the list of stock tickers from a JSON file
+            with open(self.stock_file_path, "r") as tickers_file:
+                data = json.load(tickers_file)
+                # print("data from StartApp function: ", data)
+                tickers = data["tickers"]
+                print("tickers: ", tickers)
+        except FileNotFoundError:
+            print("Stock file not found")
+            return
+        except json.JSONDecodeError:
+            print("Error decoding JSON in stock file")
+            return
             # Create an empty dictionary to store the stock data
 
         # Load your API key from a configuration file (config.json in this case)
@@ -25,9 +31,10 @@ class RunThis:
                 api_key = config.get("api_key")
         except FileNotFoundError:
             print("Configuration file not found")
+            return
         except json.JSONDecodeError:
             print("Error decoding JSON in config file")
-
+            return
         # print("API key: ", api_key)
         all_stock_data = {}
 
@@ -48,36 +55,31 @@ class RunThis:
 
 
 class StockDataAnalyzer(RunThis):
-    def __init__(self, all_stock_data, stock_file_path):
-        super().__init__()  # Call the parent class's __init__ method
-        self.all_stock_data = all_stock_data  # Initialize all_stock_data attribute
-        self.json_path = json_path  # Initialize json_path attribute
-        print("Inside the class StockDataAnalyzer")
+    def __init__(self, all_stock_data, stock_file_path, config_file_path):
+        super().__init__(stock_file_path, config_file_path)
+        self.all_stock_data = all_stock_data
 
     def get_last_n_weeks_close_prices(self, symbol, n):
-        # Get the number of periods from a JSON file
-        with open(self.stock_file_path, "r") as tickers_file:
-            data = json.load(tickers_file)
-            periods = data["periods"]
-            print("periods", periods)
+        try:
+            with open(self.stock_file_path, "r") as tickers_file:
+                data = json.load(tickers_file)
+        except FileNotFoundError:
+            print("Stock file not found")
+            return
+        except json.JSONDecodeError:
+            print("Error decoding JSON in stock file")
+            return
 
         weekly_data = self.all_stock_data[symbol]["Weekly Time Series"]
-        last_n_weeks = list(weekly_data.items())[:periods]
+        last_n_weeks = list(weekly_data.items())[:n]
 
         for week, data in last_n_weeks:
             close_price = data["4. close"]
             print(f"Week: {week}, Close Price: {close_price}")
 
 
-# Create an instance of the StockDataAnalyzer class
-# analyzer = StockDataAnalyzer(stock_data)
-
-# Get and print the last 2 weeks' close prices for TSLA
-# analyzer.get_last_n_weeks_close_prices("TSLA", n=2)
-
+# Example of usage
 app = RunThis(stock_file_path="tickers.json", config_file_path="config.json")
-app.StartApp()
-analyzer = StockDataAnalyzer(all_stock_data, stock_file_path)
-
-# Get the last 2 weeks' close prices for AAPL
+all_stock_data = app.StartApp()
+analyzer = StockDataAnalyzer(all_stock_data, "tickers.json", "config.json")
 analyzer.get_last_n_weeks_close_prices("TSLA", 2)
