98da4da2259ac6d6d79c82c4d7318caf08eefece
cooper <tokyolabs@protonmail.com>
2023-09-17 15:06:05 -0400

    wip

--- pass_data.py
+++ pass_data.py
@@ -3,7 +3,7 @@ import json
 
 class MyClass:
     def my_method(self, stock_file_path):
-        stonks = self.read_json(stock_file_path)
+        stonks = read_json(stock_file_path)
         if stonks is None:
             return None
         periods = stonks.get("periods")
@@ -12,24 +12,23 @@ class MyClass:
 
         print(f"Data received: {stock_file_path}")
 
-    def read_json(self, file_path):
-        try:
-            with open(file_path, "r") as f:
-                return json.load(f)
-        except FileNotFoundError:
-            print(f"{file_path} not found")
-            return None
-        except json.JSONDecodeError:
-            print(f"Error decoding JSON in {file_path}")
-            return None
+
+def read_json(file_path):
+    try:
+        with open(file_path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"{file_path} not found")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON in {file_path}")
+        return None
 
 
-# The function my_function should not take self as an argument unless it is part of a class.
 def my_function(stock_file_path):
     # Instantiate class within function
-    my_instance = MyClass()
 
-    stonks = my_instance.read_json(stock_file_path)
+    stonks = read_json(stock_file_path)
     if stonks is None:
         return None
     periods = stonks.get("periods")
